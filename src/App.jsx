1d1fda7acb344f29ea111117b29e68524bc54ee8
Nam Gideok <namgd1222@gmail.com>
2022-06-04 10:49:54 +0900

    필요 컴포넌트, 공통 스타일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
-  return <div>Portfolio App</div>;
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 const rootNode = document.getElementById("root");
 
-ReactDOM.createRoot(rootNode).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.createRoot(rootNode).render(<App />);
