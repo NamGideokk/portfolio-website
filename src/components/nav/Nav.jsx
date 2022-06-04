f26f385bca89224defffed2948daf54b31746a30
Nam Gideok <namgd1222@gmail.com>
2022-06-04 14:20:39 +0900

    section 나눔, nav 추가중

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./nav.css";
+import { GrHome } from "react-icons/gr";
+import { FiUser } from "react-icons/fi";
+import { BiBook } from "react-icons/bi";
+import { BiMessageSquareDots } from "react-icons/bi";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <a href="#">
+        <GrHome />
+      </a>
+      <a href="#about">
+        <FiUser />
+      </a>
+      <a href="#experience">
+        <BiBook />
+      </a>
+      <a href="#contact">
+        <BiMessageSquareDots />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
