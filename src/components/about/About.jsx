c8faa1fb236dbfa8f9bf8901dfd8ca438e1eeb08
Nam Gideok <namgd1222@gmail.com>
2022-06-05 11:33:10 +0900

    about 작성중

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,48 @@
 import React from "react";
 import "./about.css";
+import AVATAR from "../../assets/avatar.png";
+import { TbRun } from "react-icons/tb";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={AVATAR} alt="avatar" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <TbRun className="about__icon" />
+              <h5>Experience</h5>
+              <small>Rookie</small>
+            </article>
+            <article className="about__card">
+              <TbRun className="about__icon" />
+              <h5>Experience</h5>
+              <small>Rookie</small>
+            </article>
+            <article className="about__card">
+              <TbRun className="about__icon" />
+              <h5>Experience</h5>
+              <small>Rookie</small>
+            </article>
+          </div>
+
+          <p>안녕하세요 신입 개발자를 꿈꾸는 남기덕입니다.</p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
