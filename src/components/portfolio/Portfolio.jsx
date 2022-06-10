ef31e7a0ee8e8f7e60ad58e640390d0827101aa2
Nam Gideok <namgd1222@gmail.com>
2022-06-10 09:23:53 +0900

    portfolio issue 3가지 해결

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import PortfolioArticle from "./PortfolioArticle";
 import "./portfolio.scss";
-import PFIMG01 from "../../assets/pmu-logo-ex.png";
-import PFIMG02 from "../../assets/LogoHorizon.png";
-import PFIMG03 from "../../assets/twixxer_logo.png";
+import PFIMG01 from "../../assets/logo1.png";
+import PFIMG02 from "../../assets/logo2.png";
+import PFIMG03 from "../../assets/logo3.png";
 
-// issue - 상위 container와 분리되어 반응형 작동 완벽히 안함
-//       - 서로 다른 이미지 크기 맞추는 작업 필요
-//       - live demo 없는 포트폴리오 버튼 처리 필요
 const Portfolio = () => {
   return (
     <section id="portfolio">
--- src/components/portfolio/PortfolioArticle.jsx
+++ src/components/portfolio/PortfolioArticle.jsx
@@ -7,26 +7,25 @@ const PortfolioArticle = ({
   extraInfo,
   githubLink,
   demoLink,
-  extraButtonTitle = "Live Demo",
+  extraButtonTitle = "Demo",
 }) => {
   return (
-    <div className="item-wrapper">
-      <article className="portfolio__item">
-        <div className="portfolio__item-image">
-          <img src={img} alt="portfolio image" />
-        </div>
-        <h3>{title}</h3>
-        <p>{extraInfo}</p>
-        <div className="portfolio__item-cta">
-          <a href={githubLink} className="btn" target="_blank">
-            Github
-          </a>
-          <a href={demoLink} className="btn btn-primary" target="_blank">
-            {extraButtonTitle}
-          </a>
-        </div>
-      </article>
-    </div>
+    <article className="portfolio__item">
+      <div className="portfolio__item-image">
+        <img src={img} alt="portfolio image" />
+      </div>
+      <h3>{title}</h3>
+      <p>{extraInfo}</p>
+      <div className="portfolio__item-cta">
+        <a href={githubLink} className="btn" target="_blank">
+          Github
+        </a>
+        <a href={demoLink} className="btn btn-primary" target="_blank">
+          {extraButtonTitle}
+        </a>
+      </div>
+      {/* <div className="empty-bar"></div> */}
+    </article>
   );
 };
 
