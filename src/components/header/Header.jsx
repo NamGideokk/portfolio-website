7e7957a7fa5a16e7b546b29450849f3b957cfa00
Nam Gideok <namgd1222@gmail.com>
2022-06-25 14:11:48 +0900

    mobile 환경 최적화 적용중

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,37 +3,14 @@ import "./header.scss";
 import CTA from "./CTA";
 import ME from "../../assets/ngd.jpg";
 import HeaderSocials from "./HeaderSocials";
-import { useEffect } from "react";
-import { useState } from "react";
 
 const Header = () => {
-  const [vw, setVw] = useState(null);
-
-  function resizeVw() {
-    setVw(window.innerWidth);
-  }
-
-  useEffect(() => {
-    window.addEventListener("resize", resizeVw);
-    return () => {
-      window.removeEventListener("resize", resizeVw);
-    };
-  }, []);
-
   return (
     <header>
       <div className="container header__container">
         <h4>Hello. I'm Nam Gideok</h4>
 
-        {vw && !null && vw < 710 ? (
-          <h1>
-            프론트엔드 신입 개발자
-            <br />
-            남기덕입니다
-          </h1>
-        ) : (
-          <h1>프론트엔드 신입 개발자 남기덕입니다</h1>
-        )}
+        <h1>프론트엔드 신입 개발자 남기덕입니다</h1>
 
         <h3>front-end developer</h3>
 
